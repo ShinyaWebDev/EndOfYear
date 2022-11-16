9c1481342dc33386cb99865ea214e3b384f16bb1
ShinFunk <shinya.yoshida420@gmail.com>
2022-11-17 02:21:18 +1100

    good fix

--- script.js
+++ script.js
@@ -88,16 +88,16 @@ function showSlides2(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides2");
   let dots = document.getElementsByClassName("dot2");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active2", "");
   }
   slides[slideIndex2-1].style.display = "block";
-  dots[slideIndex2-1].className += " active";
+  dots[slideIndex2-1].className += " active2";
 }
 
 
